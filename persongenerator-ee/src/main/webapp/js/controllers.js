4f3f12ba00399ff064426ce3003b7006e7da6912
emiliano.goday <emiliano.goday@HP-PRO.plexus.local>
2015-01-19 11:10:38 +0100

    Añadido bootstrap

--- persongenerator-ee/src/main/webapp/js/controllers.js
+++ persongenerator-ee/src/main/webapp/js/controllers.js
@@ -1,13 +1,13 @@
-var personGeneratorApp = angular.module('personGeneratorApp', []);
+var personGeneratorApp = angular.module('personGeneratorApp', ['ui.bootstrap', 'ngStorage']);
 
-personGeneratorApp.controller('MainController', function($scope, $http) {
-	$scope.query = {
+personGeneratorApp.controller('MainController', function($scope, $http, $localStorage) {
+	$scope.query = $localStorage.$default({
 		"gender" : "",
 		"dniPattern" : "",
 		"namesPercentage" : 10,
 		"appsPercentage" : 10
-	};
-
+	});
+	
 	$scope.next = function(query) {
 		var url = 'rest/generator/next';
 		var sep = '?';
@@ -16,17 +16,21 @@ personGeneratorApp.controller('MainController', function($scope, $http) {
 			url = url + "?gender=" + query.gender;
 			sep = '&';
 		}
-		if (query.dniPattern != 'undefined') {
+
+		if (query.dniPattern != '' && query.dniPattern != null) {
 			url = url + sep + "dniPattern=" + query.dniPattern;
 			sep = '&';
 		}
 
-		url = url + sep + "namesPercentage=" + query.namesPercentage; 
+		url = url + sep + "namesPercentage=" + query.namesPercentage;
+		sep = '&';
 
 		url = url + sep + "appsPercentage=" + query.appsPercentage; 
 
 		$http.get(url).success(function(data) {
 			$scope.person = data;
+		}).error(function(data) {
+			alert(url + " " + data);
 		});
 	}
 	
